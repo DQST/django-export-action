c25d5a44046f10352c50a73e0fd14a02d7f6fed5
Dmitriy Yushko <yushko07@gmail.com>
2018-08-10 10:53:14 +0300

    Adding compatibility

--- export_action/admin.py
+++ export_action/admin.py
@@ -1,7 +1,10 @@
 import uuid
 from django.contrib import admin
 from django.contrib.contenttypes.models import ContentType
-from django.urls import reverse
+try:
+    from django.urls import reverse
+except ImportError:
+    from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.utils.translation import ugettext_lazy as _
 
